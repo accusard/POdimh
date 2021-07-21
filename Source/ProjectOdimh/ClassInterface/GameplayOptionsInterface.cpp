12d9245cdcefd61b63425a679fba0fcd4ab3622f
Vanny Sou <accusard@me.com>
2021-07-20 21:15:27 -0700

    - remove Niagara electric_arc effect from Spawner_BP and move to GridRelayLine_BP - Spawner will be renamed to Station_BP

---
+++ Source/ProjectOdimh/ClassInterface/GameplayOptionsInterface.cpp
@@ -0,0 +1,6 @@
+// Copyright 2017-2021 Vanny Sou. All Rights Reserved.
+
+
+#include "ClassInterface/GameplayOptionsInterface.h"
+
+// Add default functionality here for any IGameplayOptionsInterface functions that are not pure virtual.
--- Source/ProjectOdimh/ClassInterface/GameplayOptionsInterface.h
+++ Source/ProjectOdimh/ClassInterface/GameplayOptionsInterface.h
@@ -1,26 +1,25 @@
-// Copyright 2017-2018 Vanny Sou. All Rights Reserved.
+// Copyright 2017-2021 Vanny Sou. All Rights Reserved.
 
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GroupInterface.generated.h"
+#include "UObject/Interface.h"
+#include "GameplayOptionsInterface.generated.h"
 
 // This class does not need to be modified.
 UINTERFACE(MinimalAPI)
-class UGroupInterface : public UInterface
+class UGameplayOptionsInterface : public UInterface
 {
 	GENERATED_BODY()
 };
 
 /**
- * deprecate
+ * 
  */
-class PROJECTODIMH_API IGroupInterface
+class PROJECTODIMH_API IGameplayOptionsInterface
 {
 	GENERATED_BODY()
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
-	
-	
 };
