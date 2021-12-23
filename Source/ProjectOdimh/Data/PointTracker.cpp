d38f4fa6b42df38fceb7b300f5920d35f81302fc
Vanny Sou <accusard@me.com>
2021-12-23 08:54:43 -0800

    begin implementation of progression system

---
+++ Source/ProjectOdimh/Data/PointTracker.cpp
@@ -0,0 +1,5 @@
+// Copyright 2017-2021 Vanny Sou. All Rights Reserved.
+
+
+#include "Data/PointTracker.h"
+
---
+++ Source/ProjectOdimh/Data/PointTracker.h
@@ -0,0 +1,17 @@
+// Copyright 2017-2021 Vanny Sou. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Info.h"
+#include "PointTracker.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTODIMH_API APointTracker : public AInfo
+{
+	GENERATED_BODY()
+	
+};
